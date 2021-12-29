50faf1764bc497f446cb2e703062ec7825e1eed4
Hosein Damavandi <83751182+hoseindamavandi@users.noreply.github.com>
2021-12-29 22:30:06 +0330

    Delete Series 1 directory

--- Series 1/03/3.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a;
-    cin>>a;
-    cout<<a*(a+1)/2<<endl;
-}
--- Series 1/04/4.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    double a;
-    cin>>a;
-    cout<<(int)a<<endl;
-}
